6ad375a5c5e943d61108e70ef6218172715f228b
alexdevilmaycare <sashalevk111aaa@gmail.com>
2023-03-06 22:19:38 +0300

    Removed comments

--- makeTrain/mainTrain.cpp
+++ makeTrain/mainTrain.cpp
@@ -83,20 +83,3 @@ int main () {
 
 
 
-/* #include<deque>
-#include<iostream>
-
-void Print ( const std::deque <int> & collection ) {
-    for ( const auto & item: collection ) {
-        std::cout << item << " "; 
-    }
-    std::cout << "\n";
-}
-
-int main () {
-    std::deque <int> queuedCollection = {1, 2, 3, 4, 5};
-    Print(queuedCollection); 
-    queuedCollection.resize(3);
-    Print(queuedCollection); 
-    return 0; 
-} */ 
